29fbc450e71e36b6b34b7bc61162378b2a562b72
superdinoo <serzh.bakalov.81@mail.ru>
2023-08-27 20:36:05 +0400

    first commit

---
+++ components/Form.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Form = ({ setInputText, setTodos, todos, inputText, setStatus }) => {
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value);
+  };
+  const submitTodoHandle = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+
+  const statusHandker = (e) => {
+    setStatus(e.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          value={inputText}
+          onChange={inputTextHandler}
+          type="text"
+          className="todo-input"
+        />
+        <button
+          onClick={submitTodoHandle}
+          className="todo-button"
+          type="submit"
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandker} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ components/TodoList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div>
+      <div className="todo-container">
+        <ul className="todo-list">
+          {filteredTodos.map((todo) => (
+            <Todo
+              setTodos={setTodos}
+              key={todo.id}
+              text={todo.text}
+              todos={todos}
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
